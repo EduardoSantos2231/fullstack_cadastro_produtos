93d65d45fea124cce5797fcaf71e22f3d0cb31e8
Dudu2231 <eduardonascimento2231@gmail.com>
2025-10-18 22:22:31 -0300

    add: icone de lixeira

--- frontend_produtos/src/components/ProductContainer.tsx
+++ frontend_produtos/src/components/ProductContainer.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { deleteProduct } from "../actions/products";
 import type ProductType from "../types/ProductsType";
 import { CiEdit } from "react-icons/ci";
+import { CiTrash } from "react-icons/ci";
 import EditPopup from "./EditPopup";
 import { useProductContext } from "../contexts/productsContext";
 
@@ -27,7 +28,7 @@ export default function ProductContainer({name, onStock, id}: ProductType) {
 
         <span className="justify-end flex gap-3">
             <button className="flex items-center gap-1 p-2 hover:scale-110 duration-200 transition cursor-pointer rounded-sm bg-yellow-200" onClick={changeIsEditing}><CiEdit />Editar</button>
-            <button className="flex items-center gap-1 p-2 hover:scale-110 duration-200 transition cursor-pointer rounded-sm" onClick={deleteSingleOne}>Deletar</button>
+            <button className="flex items-center gap-1 p-2 hover:scale-110 duration-200 transition cursor-pointer rounded-sm bg-red-300" onClick={deleteSingleOne}><CiTrash />Deletar</button>
         </span>
     </div>
   )
